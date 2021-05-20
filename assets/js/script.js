77a472610f966d6254be021ae44d62635b1e190b
Andrew Bianco <andrewbianco98@gmail.com>
2021-05-20 01:55:38 -0400

    Finished the questions loop, add stop to game when timer is zero, added correct and incorrect values to buttons.

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,6 +22,8 @@ var buttons = document.querySelector(".buttons")
 var highscore = document.querySelector(".highscore");
 var input = document.querySelector(".inputControl");
 
+var timer;
+var timeLeft;
 
 function init() {
 
@@ -34,57 +36,272 @@ function startGame() {
     questionEl.classList.remove("hidden");
     buttons.classList.remove("hidden");
     timer();
-
+    nextQuestion();
 }
 
-answerButton.addEventListener("click", function question2() {
+function nextQuestion() {
+    answer1.addEventListener("click", function () {
+        if (answer1.classList.contains("correct")) {
+            question2();
+        } else {
+            subtract();
+            question2();
+        }
+    })
+    answer2.addEventListener("click", function () {
+        if (answer2.classList.contains("correct")) {
+            question2();
+        } else {
+            subtract();
+            question2();
+        }
+    })
+    answer3.addEventListener("click", function () {
+        if (answer3.classList.contains("correct")) {
+            question2();
+        } else {
+            subtract();
+            question2();
+        }
+    })
+    answer4.addEventListener("click", function () {
+        if (answer4.classList.contains("correct")) {
+            question2();
+        } else {
+            subtract();
+            question2();
+        }
+    })
+}
+
+function question2() {
     questionEl.textContent = "heelo";
     answer1.textContent = "asf";
     answer2.textContent = "asf";
     answer3.textContent = "asf";
     answer4.textContent = "asf";
 
-    answerButton.addEventListener("click", function question3() {
-        questionEl.textContent = "heelo";
-        answer1.textContent = "asf";
-        answer2.textContent = "a2";
-        answer3.textContent = "asf";
-        answer4.textContent = "asf";
+    answer2.classList.remove("correct");
+    answer4.classList.add("correct");
 
-        answerButton.addEventListener("click", function question4() {
-            questionEl.textContent = "heelo";
-            answer1.textContent = "asf";
-            answer2.textContent = "a2";
-            answer3.textContent = "a3";
-            answer4.textContent = "asf";
+    answer1.addEventListener("click", function () {
+        if (answer1.classList.contains("correct")) {
+            question3();
+        } else {
+            subtract();
+            question3();
+        }
+    })
+    answer2.addEventListener("click", function () {
+        if (answer2.classList.contains("correct")) {
+            question3();
+        } else {
+            subtract();
+            question3();
+        }
+    })
+    answer3.addEventListener("click", function () {
+        if (answer3.classList.contains("correct")) {
+            question3();
+        } else {
+            subtract();
+            question3();
+        }
+    })
+    answer4.addEventListener("click", function () {
+        if (answer4.classList.contains("correct")) {
+            question3();
+        } else {
+            subtract();
+            question3();
+        }
+    })
+}
 
-            answerButton.addEventListener("click", function question5() {
-                questionEl.textContent = "heelo";
-                answer1.textContent = "asf";
-                answer2.textContent = "a2";
-                answer3.textContent = "a3";
-                answer4.textContent = "a4";
+function question3() {
+    questionEl.textContent = "heelo";
+    answer1.textContent = "asf";
+    answer2.textContent = "a2";
+    answer3.textContent = "asf";
+    answer4.textContent = "asf";
 
-                answerButton.addEventListener("click", function question6() {
-                    questionEl.textContent = "heelo";
-                    answer1.textContent = "a1";
-                    answer2.textContent = "a2";
-                    answer3.textContent = "a3";
-                    answer4.textContent = "a4";
+    answer1.addEventListener("click", function () {
+        if (answer1.classList.contains("correct")) {
+            question4();
+        } else {
+            subtract();
+            question4();
+        }
+    })
+    answer2.addEventListener("click", function () {
+        if (answer2.classList.contains("correct")) {
+            question4();
+        } else {
+            subtract();
+            question4();
+        }
+    })
+    answer3.addEventListener("click", function () {
+        if (answer3.classList.contains("correct")) {
+            question4();
+        } else {
+            subtract();
+            question4();
+        }
+    })
+    answer4.addEventListener("click", function () {
+        if (answer4.classList.contains("correct")) {
+            question4();
+        } else {
+            subtract();
+            question4();
+        }
+    })
+}
 
-                    answerButton.addEventListener("click", function highScore() {
-                        questionEl.classList.add("hidden");
-                        buttons.classList.add("hidden");
-                        highscore.classList.remove("hidden");
-                        input.classList.remove("hidden");
-                        
-                        
-                    });
-                });
-            });
-        });
-    });
-});
+function question4() {
+    questionEl.textContent = "heelo";
+    answer1.textContent = "asf";
+    answer2.textContent = "a2";
+    answer3.textContent = "a3";
+    answer4.textContent = "asf";
+
+    answer4.classList.remove("correct");
+    answer3.classList.add("correct");
+
+    answer1.addEventListener("click", function () {
+        if (answer1.classList.contains("correct")) {
+            question5();
+        } else {
+            subtract();
+            question5();
+        }
+    })
+    answer2.addEventListener("click", function () {
+        if (answer2.classList.contains("correct")) {
+            question5();
+        } else {
+            subtract();
+            question5();
+        }
+    })
+    answer3.addEventListener("click", function () {
+        if (answer3.classList.contains("correct")) {
+            question5();
+        } else {
+            subtract();
+            question5();
+        }
+    })
+    answer4.addEventListener("click", function () {
+        if (answer4.classList.contains("correct")) {
+            question5();
+        } else {
+            subtract();
+            question5();
+        }
+    })
+}
+
+function question5() {
+    questionEl.textContent = "heelo";
+    answer1.textContent = "asf";
+    answer2.textContent = "a2";
+    answer3.textContent = "a3";
+    answer4.textContent = "a4";
+
+    answer3.classList.remove("correct");
+    answer1.classList.add("correct");
+
+    answer1.addEventListener("click", function () {
+        if (answer1.classList.contains("correct")) {
+            question6();
+        } else {
+            subtract();
+            question6();
+        }
+    })
+    answer2.addEventListener("click", function () {
+        if (answer2.classList.contains("correct")) {
+            question6();
+        } else {
+            subtract();
+            question6();
+        }
+    })
+    answer3.addEventListener("click", function () {
+        if (answer3.classList.contains("correct")) {
+            question6();
+        } else {
+            subtract();
+            question6();
+        }
+    })
+    answer4.addEventListener("click", function () {
+        if (answer4.classList.contains("correct")) {
+            question6();
+        } else {
+            subtract();
+            question6();
+        }
+    })
+}
+
+function question6() {
+    questionEl.textContent = "heelo";
+    answer1.textContent = "a1";
+    answer2.textContent = "a2";
+    answer3.textContent = "a3";
+    answer4.textContent = "a4";
+
+    answer1.classList.remove("correct");
+    answer2.classList.add("correct");
+
+    answer1.addEventListener("click", function () {
+        if (answer1.classList.contains("correct")) {
+            highScore();
+        } else {
+            subtract();
+            highScore();
+        }
+    })
+    answer2.addEventListener("click", function () {
+        if (answer2.classList.contains("correct")) {
+            highScore();
+        } else {
+            subtract();
+            highScore();
+        }
+    })
+    answer3.addEventListener("click", function () {
+        if (answer3.classList.contains("correct")) {
+            highScore();
+        } else {
+            subtract();
+            highScore();
+        }
+    })
+    answer4.addEventListener("click", function () {
+        if (answer4.classList.contains("correct")) {
+            highScore();
+        } else {
+            subtract();
+            highScore();
+        }
+    })
+}
+
+function highScore() {
+    questionEl.classList.add("hidden");
+    buttons.classList.add("hidden");
+    highscore.classList.remove("hidden");
+    input.classList.remove("hidden");
+}
+
+function subtract() {
+    timeLeft - 10;
+}
+
 
 function timer() {
     var timeLeft = 60;
@@ -94,7 +311,9 @@ function timer() {
         timerEl.textContent = "Time: " + timeLeft;
         if (timeLeft === 0) {
             clearInterval(timeInterval);
+            highScore();
         }
+        return timeLeft;
     }, 1000);
 }
 
