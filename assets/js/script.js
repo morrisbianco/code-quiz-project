c302b22860799b7adc594698a39ea1a15e919e0c
Andrew Bianco <andrewbianco98@gmail.com>
2021-05-20 21:42:07 -0400

    Fixed questions, added score, working on initials

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,319 +1,209 @@
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
+// GIVEN I am taking a code quiz DONE
+// WHEN I click the start button DONE
+// THEN a timer starts and I am presented with a question DONE
+// WHEN I answer a question DONE
+// THEN I am presented with another question DONE 
+// WHEN I answer a question incorrectly 
 // THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
+// WHEN all questions are answered or the timer reaches 0 DONE
+// THEN the game is over DONE
+// WHEN the game is over DONE
 // THEN I can save my initials and my score
 var startButtonEl = document.querySelector(".start-button");
 var questionContentEl = document.querySelector(".question-content");
 var questionEl = document.querySelector("#question");
 var timerEl = document.querySelector(".timer");
 var answerButton = document.querySelector(".button");
-var answer1 = document.querySelector(".answer1");
-var answer2 = document.querySelector(".answer2");
-var answer3 = document.querySelector(".answer3");
-var answer4 = document.querySelector(".answer4");
 var buttons = document.querySelector(".buttons")
 var highscore = document.querySelector(".highscore");
 var input = document.querySelector(".inputControl");
+var scoreText = document.querySelector("#scoreText");
+var scoreInput = document.querySelector("#input");
+var submitButton = document.querySelector(".submitButton");
 
-var timer;
 var timeLeft;
+var timeInterval;
+var stage;
+var score = 0;
+
+initials = [];
 
 function init() {
-
+getScores();
 }
 
 
 function startGame() {
     startButtonEl.classList.add("hidden");
     questionContentEl.classList.add("hidden");
-    questionEl.classList.remove("hidden");
-    buttons.classList.remove("hidden");
+    stage = 0;
     timer();
-    nextQuestion();
+    firstQuestion();
+    
 }
 
-function nextQuestion() {
-    answer1.addEventListener("click", function () {
-        if (answer1.classList.contains("correct")) {
-            question2();
-        } else {
-            subtract();
-            question2();
-        }
-    })
-    answer2.addEventListener("click", function () {
-        if (answer2.classList.contains("correct")) {
-            question2();
-        } else {
-            subtract();
-            question2();
-        }
-    })
-    answer3.addEventListener("click", function () {
-        if (answer3.classList.contains("correct")) {
-            question2();
-        } else {
-            subtract();
-            question2();
-        }
-    })
-    answer4.addEventListener("click", function () {
-        if (answer4.classList.contains("correct")) {
-            question2();
-        } else {
-            subtract();
-            question2();
-        }
-    })
+var questions = [
+    {
+        question: "What method is used to create a copy of some or all of the values within an array?",
+        answers: [
+            "Slice",
+            "Splice",
+            "Space",
+            "Spike",
+        ]
+    },
+    {
+        question: "What does DOM stand for?",
+        answers: [
+            "Document Obtuse Medal",
+            "Document Oblique Model",
+            "Document Object Mode",
+            "Document Object Model",
+        ]
+    },
+    {
+        question: "Event.stopPropagation does what?",
+        answers: [
+            "Removes Children from Parent Div's",
+            "Stops the code from refreshing the page",
+            "Prevents Bubbling",
+            "Registers where the user clicked",
+        ]
+    },
+    {
+        question: "What is the purpose of .trim()",
+        answers: [
+            "Removes excess whitespace from the input",
+            "Deletes the last item in an array",
+            "Removes the string value from numbers",
+            "Removes the first item in an array",
+        ]
+    },
+    {
+        question: "What does API stand for?",
+        answers: [
+            "Application Programming Internet",
+            "Application Programming Interface",
+            "Application Programming Intercom",
+            "Application Progressing Interface",
+        ]
+    },
+    {
+        question: "What HTML tag is used to link the Javascript code to the HTML document?",
+        answers: [
+            "Meta",
+            "Link",
+            "Script",
+            "Title",
+        ]
+    },
+]
+
+function firstQuestion() {
+    while (buttons.firstChild) {
+        buttons.removeChild(buttons.firstChild);
+    }
+    showQuestion();
 }
 
-function question2() {
-    questionEl.textContent = "heelo";
-    answer1.textContent = "asf";
-    answer2.textContent = "asf";
-    answer3.textContent = "asf";
-    answer4.textContent = "asf";
-
-    answer2.classList.remove("correct");
-    answer4.classList.add("correct");
-
-    answer1.addEventListener("click", function () {
-        if (answer1.classList.contains("correct")) {
-            question3();
-        } else {
-            subtract();
-            question3();
-        }
-    })
-    answer2.addEventListener("click", function () {
-        if (answer2.classList.contains("correct")) {
-            question3();
-        } else {
-            subtract();
-            question3();
-        }
-    })
-    answer3.addEventListener("click", function () {
-        if (answer3.classList.contains("correct")) {
-            question3();
-        } else {
-            subtract();
-            question3();
-        }
-    })
-    answer4.addEventListener("click", function () {
-        if (answer4.classList.contains("correct")) {
-            question3();
-        } else {
-            subtract();
-            question3();
-        }
-    })
+function showQuestion() {
+    questionEl.textContent = questions[stage].question;
+    
+    for (var i = 0; i < questions[stage].answers.length; i++) {
+        var answer = questions[stage].answers[i];
+        var buttonEl = document.createElement("button");
+        buttonEl.textContent = answer;
+        buttonEl.setAttribute("class", "button");
+        buttonEl.setAttribute("data-index", i);
+        buttons.appendChild(buttonEl);
+    
+    }
+    
+    
 }
 
-function question3() {
-    questionEl.textContent = "heelo";
-    answer1.textContent = "asf";
-    answer2.textContent = "a2";
-    answer3.textContent = "asf";
-    answer4.textContent = "asf";
+buttons.addEventListener("click", function(event) {
+    var element = event.target;
+    if (element.matches("button")) {
+        if (stage === questions.length - 1) {
+            highScore();
+        } else if (stage < questions.length) {
+            stage++;
+            firstQuestion();
+        } 
+    }
+    
+});
 
-    answer1.addEventListener("click", function () {
-        if (answer1.classList.contains("correct")) {
-            question4();
-        } else {
-            subtract();
-            question4();
-        }
-    })
-    answer2.addEventListener("click", function () {
-        if (answer2.classList.contains("correct")) {
-            question4();
-        } else {
-            subtract();
-            question4();
-        }
-    })
-    answer3.addEventListener("click", function () {
-        if (answer3.classList.contains("correct")) {
-            question4();
-        } else {
-            subtract();
-            question4();
-        }
-    })
-    answer4.addEventListener("click", function () {
-        if (answer4.classList.contains("correct")) {
-            question4();
-        } else {
-            subtract();
-            question4();
-        }
-    })
+function checkCorrect () {
+    
 }
 
-function question4() {
-    questionEl.textContent = "heelo";
-    answer1.textContent = "asf";
-    answer2.textContent = "a2";
-    answer3.textContent = "a3";
-    answer4.textContent = "asf";
+// buttons.addEventListener("click", function(event) {
+//     var element = event.target;
+//     if (element.matches("button")) {
+//         var index = parseInt(element.dataset.index);
+//         console.log(index, question.correct);
+//         if (element.matches("incorrect")) {
 
-    answer4.classList.remove("correct");
-    answer3.classList.add("correct");
-
-    answer1.addEventListener("click", function () {
-        if (answer1.classList.contains("correct")) {
-            question5();
-        } else {
-            subtract();
-            question5();
-        }
-    })
-    answer2.addEventListener("click", function () {
-        if (answer2.classList.contains("correct")) {
-            question5();
-        } else {
-            subtract();
-            question5();
-        }
-    })
-    answer3.addEventListener("click", function () {
-        if (answer3.classList.contains("correct")) {
-            question5();
-        } else {
-            subtract();
-            question5();
-        }
-    })
-    answer4.addEventListener("click", function () {
-        if (answer4.classList.contains("correct")) {
-            question5();
-        } else {
-            subtract();
-            question5();
-        }
-    })
-}
-
-function question5() {
-    questionEl.textContent = "heelo";
-    answer1.textContent = "asf";
-    answer2.textContent = "a2";
-    answer3.textContent = "a3";
-    answer4.textContent = "a4";
-
-    answer3.classList.remove("correct");
-    answer1.classList.add("correct");
-
-    answer1.addEventListener("click", function () {
-        if (answer1.classList.contains("correct")) {
-            question6();
-        } else {
-            subtract();
-            question6();
-        }
-    })
-    answer2.addEventListener("click", function () {
-        if (answer2.classList.contains("correct")) {
-            question6();
-        } else {
-            subtract();
-            question6();
-        }
-    })
-    answer3.addEventListener("click", function () {
-        if (answer3.classList.contains("correct")) {
-            question6();
-        } else {
-            subtract();
-            question6();
-        }
-    })
-    answer4.addEventListener("click", function () {
-        if (answer4.classList.contains("correct")) {
-            question6();
-        } else {
-            subtract();
-            question6();
-        }
-    })
-}
-
-function question6() {
-    questionEl.textContent = "heelo";
-    answer1.textContent = "a1";
-    answer2.textContent = "a2";
-    answer3.textContent = "a3";
-    answer4.textContent = "a4";
-
-    answer1.classList.remove("correct");
-    answer2.classList.add("correct");
-
-    answer1.addEventListener("click", function () {
-        if (answer1.classList.contains("correct")) {
-            highScore();
-        } else {
-            subtract();
-            highScore();
-        }
-    })
-    answer2.addEventListener("click", function () {
-        if (answer2.classList.contains("correct")) {
-            highScore();
-        } else {
-            subtract();
-            highScore();
-        }
-    })
-    answer3.addEventListener("click", function () {
-        if (answer3.classList.contains("correct")) {
-            highScore();
-        } else {
-            subtract();
-            highScore();
-        }
-    })
-    answer4.addEventListener("click", function () {
-        if (answer4.classList.contains("correct")) {
-            highScore();
-        } else {
-            subtract();
-            highScore();
-        }
-    })
-}
+//         }
+//     }
+// })
 
 function highScore() {
     questionEl.classList.add("hidden");
     buttons.classList.add("hidden");
     highscore.classList.remove("hidden");
     input.classList.remove("hidden");
+    clearInterval(timeInterval)
+    score = timerEl.textContent
+    setScores();
+    scoreText.textContent = ("Your final score is " + score + ".");
+
 }
 
 function subtract() {
-    timeLeft - 10;
+        timeLeft -= 10;
+        timerEl.textContent = timeLeft;
+
+        // if (timeLeft < 0) {
+        //     clearInterval(timeInterval);
+        //     timerEl.textContent = "Time: " + 0;
+        //     highScore();
+        // }
+
 }
+
+submitButton.addEventListener("submit", function(event) {
+    event.preventDefault();
+    var inputText = scoreInput.value.trim();
+    if (inputText === "") {
+      return;
+    }
+    initials.push(inputText);
+    scoreInput.value = "";
+
+    console.log(inputText);
+   
+    // TODO: What will happen when the following functions are called?
+    // The storeTodos stores the todos in the local storage
+    // the renderTodos writes them out on page
+    // storeTodos();
+    // renderTodos();
+  });
 
 
 function timer() {
-    var timeLeft = 60;
-    timerEl.textContent = "Time: " + timeLeft;
-    var timeInterval = setInterval(function () {
+    timeLeft = 60;
+    timerEl.textContent = timeLeft;
+    timeInterval = setInterval(function () {
         timeLeft--;
-        timerEl.textContent = "Time: " + timeLeft;
+        timerEl.textContent = timeLeft;
         if (timeLeft === 0) {
             clearInterval(timeInterval);
             highScore();
         }
-        return timeLeft;
+
     }, 1000);
 }
 
@@ -323,7 +213,8 @@ function getScores() {
 }
 
 function setScores() {
-
+    timerEl.textContent = score;
+    localStorage.setItem("score", JSON.stringify(score));
 }
 
 startButtonEl.addEventListener("click", startGame);
